46a186cc58210a2ef0f8b72817069acab611c0a5
Louise <louise.cch@gmail.com>
2025-09-16 11:11:29 +0300

    add netlify adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   // Platform-specific build configuration
   build: {
     // Ensure consistent builds across platforms
